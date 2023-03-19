5eb1b2fd7ba4665d51a0c560fd294791c2ef90db
gabrielmds222 <gabrielmedsilva@outlook.com>
2023-03-19 18:52:57 -0300

    feat: Estrutura dos itens

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,14 @@
 import "./App.css";
 
 export default function App() {
-  return <div>Hello world</div>;
+  return (
+    <div className="container">
+      <div className="lista-produtos">
+        <h2>Carrinho de compras</h2>
+        <p>Você tem 6 itens</p>
+        <div className="produtos" style={{ border: "3px solid red" }}></div>
+        <div className="resumo" style={{ border: "3px solid purple" }}></div>
+      </div>
+    </div>
+  );
 }
