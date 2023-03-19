1a3d0d84b9de4d899002f7f4876928c7b5d150d1
gabrielmds222 <gabrielmedsilva@outlook.com>
2023-03-19 19:05:58 -0300

    feat: Criação dos arquivos de componentes

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,20 @@
 import "./App.css";
 
+import Produtos from "./components/Produtos";
+import Resumo from "./components/Resumo";
+
 export default function App() {
   return (
     <div className="container">
       <div className="lista-produtos">
         <h2>Carrinho de compras</h2>
         <p>Você tem 6 itens</p>
-        <div className="produtos" style={{ border: "3px solid red" }}></div>
-        <div className="resumo" style={{ border: "3px solid purple" }}></div>
+        <div className="produtos" style={{ border: "3px solid red" }}>
+          <Produtos />
+        </div>
+        <div className="resumo" style={{ border: "3px solid purple" }}>
+          <Resumo />
+        </div>
       </div>
     </div>
   );
---
+++ src/components/Resumo/index.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Resumo = () => {
+  return (
+    <div>
+      <h1>Resumo do pedido</h1>
+    </div>
+  );
+};
+
+export default Resumo;
