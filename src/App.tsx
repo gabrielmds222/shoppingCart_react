0c34061881fbbabe9c55175ec551884e648aeccd
gabrielmds222 <gabrielmedsilva@outlook.com>
2023-03-22 21:42:04 -0300

    feat: ajustes

--- src/App.tsx
+++ src/App.tsx
@@ -9,10 +9,10 @@ export default function App() {
       <div className="lista-produtos">
         <h2>Carrinho de compras</h2>
         <p>Você tem 6 itens</p>
-        <div className="produtos" style={{ border: "3px solid red" }}>
+        <div className="produtos" style={{ border: "3px solid #000" }}>
           <Produtos />
         </div>
-        <div className="resumo" style={{ border: "3px solid purple" }}>
+        <div className="resumo" style={{ border: "3px solid #000" }}>
           <Resumo />
         </div>
       </div>
--- src/components/Produtos/index.tsx
+++ src/components/Produtos/index.tsx
@@ -3,7 +3,7 @@ import React from "react";
 const Produtos = () => {
   return (
     <div>
-      <h1>Lista de produtos</h1>
+      <h1>produtos</h1>
     </div>
   );
 };
