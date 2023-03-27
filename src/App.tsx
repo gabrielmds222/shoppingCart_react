1e7152f7ef22f56b1b5a3e9da4481c79d2162c6f
gabrielmds222 <gabrielmedsilva@outlook.com>
2023-03-26 23:10:38 -0300

    feat: css da tela

--- src/App.tsx
+++ src/App.tsx
@@ -5,14 +5,14 @@ import Resumo from "./components/Resumo";
 
 export default function App() {
   return (
-    <div className="container">
+    <div className="container" style={{ border: "3px solid #000" }}>
       <div className="lista-produtos">
         <h2>Carrinho de compras</h2>
         <p>Você tem 6 itens</p>
-        <div className="produtos" style={{ border: "3px solid #000" }}>
+        <div className="produtos">
           <Produtos />
         </div>
-        <div className="resumo" style={{ border: "3px solid #000" }}>
+        <div className="resumo">
           <Resumo />
         </div>
       </div>
