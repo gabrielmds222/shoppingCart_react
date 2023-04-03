d417e34bb02d6d49eb3c5d447f8c43d81aaa8a81
gabrielmds222 <gabrielmedsilva@outlook.com>
2023-04-03 18:30:52 -0300

    feat: Dados mockados para utilizar no frontend

---
+++ src/data/index.ts
@@ -0,0 +1,29 @@
+import Pizza from "../../assets/imgPizza.jpg";
+// Arquivo de imagens
+import Product from "../components/Produto/interfaces";
+
+const products: Product[] = [
+  {
+    id: 1,
+    img: Pizza,
+    name: "Pizza",
+    description: "Pizza do boy Óliver",
+    price: 59.99,
+  },
+  {
+    id: 2,
+    img: Pizza,
+    name: "Pizza",
+    description: "Pizza do boy Óliver",
+    price: 59.99,
+  },
+  {
+    id: 3,
+    img: Pizza,
+    name: "Pizza",
+    description: "Pizza do boy Óliver",
+    price: 59.99,
+  },
+];
+
+export default products;
