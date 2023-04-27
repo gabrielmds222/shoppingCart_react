34417138f30ae941f0d16abfdbedd024f77355f3
gabrielmds222 <gabrielmedsilva@outlook.com>
2023-04-27 20:52:52 -0300

    fix: Corrige erros de tipagem

--- src/components/Resumo/index.tsx
+++ src/components/Resumo/index.tsx
@@ -1,26 +1,58 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import { Repeat } from "@phosphor-icons/react";
 
 import "./style.css";
 
+import api from "../../api";
+
+type ResponseCepDataType = {
+  cep: string;
+  logradouro: string;
+  complemento: string;
+  bairro: string;
+  localidade: string;
+  uf: string;
+  ibge: string;
+  gia: string;
+  ddd: string;
+  siafi: string;
+};
+
 const Resumo = () => {
+  const [user, setUser] = useState<ResponseCepDataType>();
+  const [cpfDigitado, setCpfDigitado] = useState<string>("");
+
+  async function handleCep(e: any) {
+    e.preventDefault();
+    api
+      .get(`/${cpfDigitado}/json`)
+      .then((response) => setUser(response.data))
+      .catch((err) => {
+        console.error("ops! ocorreu um erro" + err);
+      });
+  }
+
   return (
     <>
       <div className="box">
-        <header>Resumo</header>
+        <header>Resumo: {user?.localidade}</header>
 
         <div className="info">
           <div>
-            <span>Frete</span>
+            <span>Frete: {user?.uf}</span>
           </div>
           <div>
             <span>CEP</span>
-            <div className="frete">
-              <input type="text" />
-              <button>
+            <form className="frete" onSubmit={handleCep}>
+              <input
+                type="text"
+                value={cpfDigitado}
+                onChange={(e) => setCpfDigitado(e.target.value)}
+              />
+              <button type="submit">
                 <Repeat size={24} color="#fff" weight="bold" />
               </button>
-            </div>
+            </form>
             <span>Preço</span>
           </div>
           <div>
