bd708f787bc5aaf96a517db2e60665e20dc33f2b
gabrielmds222 <gabrielmedsilva@outlook.com>
2023-05-02 17:42:11 -0300

    feat: Cria página para armazenar o código da página principal

---
+++ src/pages/index.tsx
@@ -0,0 +1,41 @@
+import React, { useState, useContext } from "react";
+
+import Produto from "../components/Produto";
+import Resumo from "../components/Resumo";
+
+import { QuantidadeItemContext } from "../contexts/QuantidadeItems";
+
+const CarrinhoDeCompras = () => {
+  const { products, setProducts, quantidadeTotal } = useContext(
+    QuantidadeItemContext
+  );
+  return (
+    <main>
+      <div className="page-title">
+        <h2>Carrinho de compras</h2>
+        <p>Você tem {quantidadeTotal} itens</p>
+      </div>
+      <div className="content">
+        <section>
+          <table>
+            <thead>
+              <tr>
+                <th colSpan={4}></th>
+              </tr>
+            </thead>
+            <tbody>
+              {products.length > 0
+                ? products.map((product) => <Produto product={product} />)
+                : "Nenhum item no carrinho"}
+            </tbody>
+          </table>
+        </section>
+        <aside>
+          <Resumo />
+        </aside>
+      </div>
+    </main>
+  );
+};
+
+export default CarrinhoDeCompras;
