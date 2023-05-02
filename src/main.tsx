aee719be092ae14c084973f6ff8e786984afb682
gabrielmds222 <gabrielmedsilva@outlook.com>
2023-05-02 17:44:21 -0300

    rm: Remove o provider do main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -2,12 +2,9 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
 import "./index.css";
-import { QuantidadeItemProvider } from "./contexts/QuantidadeItems";
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
-    <QuantidadeItemProvider>
-      <App />
-    </QuantidadeItemProvider>
+    <App />
   </React.StrictMode>
 );
