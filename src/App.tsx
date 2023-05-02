fdaaf4e0ded82e71f1eddd166e9cf2b90629b449
gabrielmds222 <gabrielmedsilva@outlook.com>
2023-05-02 17:46:16 -0300

    rm: Remove código principal da tela

--- src/App.tsx
+++ src/App.tsx
@@ -1,50 +1,12 @@
-import React, { useState } from "react";
 import "./App.css";
 
-import Produto from "./components/Produto";
-import Resumo from "./components/Resumo";
-
-import defaultProducts from "./data/product";
+import { QuantidadeItemProvider } from "./contexts/QuantidadeItems";
+import CarrinhoDeCompras from "./pages";
 
 export default function App() {
-  const [products, setProducts] = useState(defaultProducts);
-
-  function handleDelete(id: number) {
-    setProducts(products.filter((el) => el.id !== id));
-  }
-
   return (
-    <>
-      <main>
-        <div className="page-title">
-          <h2>Carrinho de compras</h2>
-          <p>Você tem X itens</p>
-        </div>
-        <div className="content">
-          <section>
-            <table>
-              <thead>
-                <tr>
-                  <th></th>
-                  <th></th>
-                  <th></th>
-                  <th></th>
-                </tr>
-              </thead>
-              <tbody>
-                {products.length > 0
-                  ? products.map((product) => (
-                      <Produto product={product} handleDelete={handleDelete} />
-                    ))
-                  : "Nenhum item no carrinho"}
-              </tbody>
-            </table>
-          </section>
-          <aside>
-            <Resumo />
-          </aside>
-        </div>
-      </main>
-    </>
+    <QuantidadeItemProvider>
+      <CarrinhoDeCompras />
+    </QuantidadeItemProvider>
   );
 }
