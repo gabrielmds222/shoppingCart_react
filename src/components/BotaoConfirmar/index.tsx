cb18138dec13b5b5a209cb45062aa25649e2164a
gabrielmds222 <gabrielmedsilva@outlook.com>
2023-05-04 22:23:34 -0300

    feat: Separa o botão de confirmar em um componente

---
+++ src/components/BotaoConfirmar/index.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+import "./styles.css";
+
+const BotaoConfirmar = () => {
+  return (
+    <>
+      <button className="btn" type="submit">
+        <span className="text">Fechar Pedido</span>
+      </button>
+    </>
+  );
+};
+
+export default BotaoConfirmar;
